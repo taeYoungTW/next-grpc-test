519a51583fc690d11c0318037a7cbd00c05d8684
TaeYoung Kim <taezero@twinny.ai>
2022-06-21 14:56:31 +0900

    SSR test
    1. grpc-tools, grpc_tools_node_protoc_ts
    설치 필요
    2. protoc3 설치 전제

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+    reactStrictMode: true,
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'src', 'styles')],
+        prependData: `@import "./variables/index.scss";`,
+    },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
